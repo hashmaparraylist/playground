fbe6b5b994b5ced621359c276a77fd0a7776e7e5
Sebastian Qu <sebastianqu@gmail.com>
2017-03-23 17:57:39 +0800

    fix bug

--- node/node-voice/index.js
+++ node/node-voice/index.js
@@ -6,14 +6,16 @@ var api = 'http://api.hivoice.cn/USCService/WebApi';
 var appid = '';
 var appKey = '';
 
+
 var files= [
-  '8k.wav',
-  'FkElvx9ailgxGK5TgY3_E6pYp72l.2',
-  'FkElvx9ailgxGK5TgY3_E6pYp72l.1',
-  'FkElvx9ailgxGK5TgY3_E6pYp72l',
   'story-58d28500f50ec954516fe3c2.mp3',
-  'FoGfRbrL-_MiQQjm15kLE6Pqseti',
-  'story-58d2840ff50ec954516fe3be.mp3'
+  '16k.wav',
+  '8k.wav'
+  //'FkElvx9ailgxGK5TgY3_E6pYp72l.2',
+  //'FkElvx9ailgxGK5TgY3_E6pYp72l.1',
+  //'FkElvx9ailgxGK5TgY3_E6pYp72l',
+  //'FoGfRbrL-_MiQQjm15kLE6Pqseti',
+  //'story-58d2840ff50ec954516fe3be.mp3'
 ];
 
 var postOptions = {
@@ -25,7 +27,7 @@ var postOptions = {
     'Accept': 'text/plain',
     'Accept-Language': 'zh-CN',
     'Accept-Charset': 'utf-8',
-    'Transfer-Encoding': 'chunked',
+    //'Transfer-Encoding': 'chunked',
     'Accept-Topic': 'general'
   },
   method: 'POST'
@@ -36,31 +38,35 @@ var MAX = 640;
 files.forEach(function(file) {
   var buffer = fs.readFileSync('./temp/' + file);
 
+//  postOptions.headers['Content-Length'] = buffer.length;
+  //console.log(postOptions);
   var stream = fs.createReadStream('./temp/' + file);
   var request = http.request(postOptions, function(res) {
-    console.log('transfer ' + file + ' status: ' + res.statusCode);
+   // console.log('transfer ' + file + ' status: ' + res.statusCode);
     var body = ''
     res.setEncoding('utf8');
     res.on('data', function(chunk) {
-      console.log('transfer ' + file + ' status: ' + res.statusCode + ' body: [' + chunk.toString() + ']');
       body += chunk;
     });
     res.on('end', function() {
-      console.log('body:' + body);
+      console.log('transfer ' + file + ' status: ' + res.statusCode + ' body: [' + body + ']');
     });
   }).on('error', function(error) {
     console.log(' error: [' + error.message + ']');
   });
 
+  /*
   var length = buffer.length;
   var offset = 0;
   while (offset < length) {
     var size = (offset + MAX) >= length ? (length - offset): MAX;
-    //console.log('upload offset :' + offset + ' size: ' + size);
+    console.log('upload offset :' + offset + ' size: ' + size);
     var upload = Buffer.from(buffer, offset, size);
-    request.write(upload);
     offset += MAX;
   }
+  */
+  request.write(buffer);
+
   request.end();
 });
 
